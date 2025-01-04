f7ecd67162463b0dcae351f47283cc5549ac69d3
NurgaliyevS <nurgasab@gmail.com>
2025-01-04 20:17:28 +0500

    upd

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -3,22 +3,22 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header className="bg-white bg-opacity-80 backdrop-blur-md shadow-sm">
-      <nav className="container mx-auto px-6 py-3">
+      <nav className="container mx-auto px-6 py-6">
         <div className="flex justify-between items-center">
           <Link href="/" className="text-2xl font-bold text-purple-800">
             Adaptogen Blend
           </Link>
           <div className="flex items-center space-x-4">
-            <Link href="/" className="text-gray-700 hover:text-purple-600">
+            <Link href="/" className="text-gray-700 hover:text-purple-600 text-base">
               Home
             </Link>
             <Link
               href="/products"
-              className="text-gray-700 hover:text-purple-600"
+              className="text-gray-700 hover:text-purple-600 text-base"
             >
               Products
             </Link>
-            <Link href="/blog" className="text-gray-700 hover:text-purple-600">
+            <Link href="/blog" className="text-gray-700 hover:text-purple-600 text-base">
               Blog
             </Link>
           </div>
